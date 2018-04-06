987c02d11c05569087c7ff34d4bc0d3b6f85e6a4
Unknown <julianvu@outlook.com>
2018-04-05 22:54:07 -0700

    Added functional LoginView

---
+++ src/views/LoginView.java
@@ -0,0 +1,70 @@
+package views;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class LoginView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Login");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        final Text signInMessage = new Text();
+        grid.add(signInMessage, 1, 6);
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+
+        Text sceneTitle = new Text("Login credentials required");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        Label username = new Label("Username:");
+        grid.add(username, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label password = new Label("Password:");
+        grid.add(password, 0, 2);
+
+        PasswordField passwordField = new PasswordField();
+        grid.add(passwordField, 1, 2);
+
+        Button signIn = new Button("Sign in");
+        HBox hboxButton = new HBox(10);
+        hboxButton.setAlignment(Pos.BOTTOM_RIGHT);
+        hboxButton.getChildren().addAll(signIn);
+        grid.add(hboxButton, 1, 4);
+
+        signIn.setOnAction(event -> {
+            signInMessage.setFill(Color.RED);
+            signInMessage.setText("Sign in failed.");
+        });
+
+        primaryStage.show();
+    }
+}
