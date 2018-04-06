e4fc6d1f6d7baa7b342b44f09697f223f306ecaf
Unknown <julianvu@outlook.com>
2018-04-06 07:05:16 -0700

    Changed typeface of LoginView and made "Enter" key submit form

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -39,22 +40,37 @@ public class LoginView extends Application {
         primaryStage.setScene(scene);
 
         Text sceneTitle = new Text("Login credentials required");
-        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        sceneTitle.setFont(Font.font("Helvetica Neue", FontWeight.THIN, 20));
         grid.add(sceneTitle, 0, 0, 2, 1);
 
         Label username = new Label("Username:");
+        username.setFont(Font.font("Helvetica Neue", FontWeight.THIN, 15));
         grid.add(username, 0, 1);
 
         TextField userTextField = new TextField();
+        userTextField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                signInMessage.setFill(Color.RED);
+                signInMessage.setText("Sign in failed.");
+            }
+        });
         grid.add(userTextField, 1, 1);
 
         Label password = new Label("Password:");
+        password.setFont(Font.font("Helvetica Neue", FontWeight.THIN, 15));
         grid.add(password, 0, 2);
 
         PasswordField passwordField = new PasswordField();
+        passwordField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                signInMessage.setFill(Color.RED);
+                signInMessage.setText("Sign in failed.");
+            }
+        });
         grid.add(passwordField, 1, 2);
 
         Button signIn = new Button("Sign in");
+        signIn.setFont(Font.font("Helvetica Neue", FontWeight.THIN, 15));
         HBox hboxButton = new HBox(10);
         hboxButton.setAlignment(Pos.BOTTOM_RIGHT);
         hboxButton.getChildren().addAll(signIn);
@@ -63,6 +79,13 @@ public class LoginView extends Application {
         signIn.setOnAction(event -> {
             signInMessage.setFill(Color.RED);
             signInMessage.setText("Sign in failed.");
+        });
+
+        signIn.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                signInMessage.setFill(Color.RED);
+                signInMessage.setText("Sign in failed.");
+            }
         });
 
         primaryStage.show();
