1b8dbaf97b7d1cb95123fadb0ee5c951317b0e30
Unknown <julianvu@outlook.com>
2018-04-05 22:53:29 -0700

    Initialized starter files

---
+++ src/views/TaskView.java
@@ -0,0 +1,16 @@
+package views;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TaskView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
