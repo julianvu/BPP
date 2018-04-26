ddea428d9a5b067448725b64bc598961c385230d
Julian Vu <julianvu@outlook.com>
2018-04-26 07:41:47 -0700

    Changed instance variables to private, created getters/setters
    Editting a task will be the role of its ViewController. The ViewController will determine if it wants to change the name, the description, or the due date.

--- src/models/Task.java
+++ src/models/Task.java
@@ -1,9 +1,9 @@
 package models;
 
 public class Task {
-	String name = "";
-	String description = "";
-	String date = "";
+	private String name;
+	private String description;
+	private String date;
 	
 	public Task(String name, String description, String date) {
 		this.name = name;
@@ -11,26 +11,31 @@ public class Task {
 		this.date = date;
 	}
 	
-	public void editTask(String name, String description, String date) {
-		
-		if(!name.equals("")) {
-			this.name = name;
-		}
-		
-		if(!description.equals("")) {
-			this.description = description;
-		}
-		
-		if(!date.equals("")) {
-			this.date = date;
-		}
-	}
-	
 	public String getName() {
 		return name;
 	}
-	
-	public int compareTo(Task other) {
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int compareTo(Task other) {
 		if(name.toUpperCase().equals(other.name.toUpperCase())) {
 			return 0;
 		}
