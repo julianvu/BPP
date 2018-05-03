6f09522c577f72d10b2d08d67926962a5d648965
Julian Vu <julianvu@outlook.com>
2018-05-03 11:25:01 -0700

    Refactored TaskView to TaskViewController and added test to MainScreen

--- src/views/MainScreen.java
+++ src/views/MainScreen.java
@@ -1,7 +1,12 @@
 package views;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import models.Task;
+
+import java.time.LocalDate;
 
 public class MainScreen extends Application {
 
@@ -11,6 +16,13 @@ public class MainScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Task task = new Task("Rewrite LoginViewController", "LoginViewController needs to be rewritten.", LocalDate.now());
+        TaskViewController testTaskView = new TaskViewController(task);
+        BorderPane pane = new BorderPane();
+        pane.setCenter(testTaskView);
 
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
